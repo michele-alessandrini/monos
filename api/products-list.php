e7dfed27131e91d6845037fc21b9fbf3e40fdcc2
Michele Alessandrini <michele.alessandrini@gmail.com>
2022-01-04 17:03:18 +0100

    change products-list

--- api/products-list.php
+++ api/products-list.php
@@ -7,20 +7,26 @@
   $username = $_SERVER['MONOS_DB_UID'];
   $password = $_SERVER['MONOS_DB_PWD'];
 
-  $connection = mysqli_connect($dbhost, $username, $password, $dbname, $dbport);
+  try {
+      // Try Connect to the DB with new MySqli object - Params {hostname, userid, password, dbname}
+      $mysqli = new mysqli($dbhost, $username, $password, $dbname, $dbport);
 
-  $sql = "SELECT * from products";
 
-  $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+      $statement = $mysqli->prepare("SELECT * from products");
 
-  $emparray = array();
-  while($row =mysqli_fetch_assoc($result))
-  {
-        $emparray[] = $row;
+
+      $statement->execute(); // Execute the statement.
+      $result = $statement->get_result(); // Binds the last executed statement as a result.
+
+      echo json_encode(($result->fetch_assoc())); // Parse to JSON and print.
+
+  } catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
+      echo "MySQLi Error Code: " . $e->getCode() . "<br />";
+      echo "Exception Msg: " . $e->getMessage();
+      exit(); // exit and close connection.
   }
 
-  echo json_encode($emparray);
+  $mysqli->close(); // finally, close the connection
 
-  //close the db connection
-  mysqli_close($connection);
+
 ?>
