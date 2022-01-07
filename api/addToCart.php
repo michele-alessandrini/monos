081973a7e30dc416ce4735c779b343b06c9220df
Michele Alessandrini <michele.alessandrini@gmail.com>
2022-01-07 15:53:42 +0100

    add to cart PHP

---
+++ api/addToCart.php
@@ -0,0 +1,31 @@
+<?php
+
+  require __DIR__ . '/vendor/autoload.php';
+
+  sleep($_SERVER['DELAY_AJAX_IN_SEC']);
+
+  $dbhost = $_SERVER['MONOS_DB_HOSTNAME'];
+  $dbport = $_SERVER['MONOS_DB_PORT'];
+  $dbname = $_SERVER['MONOS_DB_NAME'];
+  $charset = 'utf8' ;
+
+  $username = $_SERVER['MONOS_DB_UID'];
+  $password = $_SERVER['MONOS_DB_PWD'];
+
+  $options = array(
+    'cluster' => 'eu',
+    'useTLS' => true
+  );
+
+  $pusher = new Pusher\Pusher(
+    '630cbbbb35f56d8c042d',
+    '0908f6e4c160170defe2',
+    '1327975',
+    $options
+  );
+
+  $data['message'] = rand(1, 10);
+  $pusher->trigger('monos', 'my-shopping', $data);
+?>
+
+?>
